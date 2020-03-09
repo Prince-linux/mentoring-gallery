c6a7fea11078b0dd16050dac7aa9aa1d6c71314c
Prince Boamah <princeboamah@Princes-MacBook-Air.local>
2020-03-09 17:02:04 +0000

    Prince: renamed home to index and passed a meessage to template

--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
+def index(request):
     # return HttpResponse('Hello, World')
-    return render(request, 'gallery/index.html')
+    return render(request, 'gallery/index.html', {'message': 'Welcome to my gallery'})
 
