b5ecb7b840b5fde14c1978b62a3989e55362fb82
Prince Boamah <princeboamah@Princes-MacBook-Air.local>
2020-03-10 13:57:11 +0000

    Prince: Added a base template

--- gallery/views.py
+++ gallery/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse('Hello, World')
-    return render(request, 'gallery/index.html', {'message': 'Welcome to my gallery'})
+    return render(request, 'gallery/index.html', {'message': 'Hello World!'})
 
